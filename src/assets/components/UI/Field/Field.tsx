e52da85d98ad3aecef270e4d55ecb1e706fd2352
kuropa <drumminde@gmail.com>
2026-03-04 12:20:58 +0300

    fix

--- src/assets/components/UI/Field/Field.tsx
+++ src/assets/components/UI/Field/Field.tsx
@@ -1,5 +1,27 @@
 import Btn from '../Btn/Btn.tsx';
 import styles from './Field.module.css';
+import plusImg from '/src/assets/img/plus.svg';
+import minusImg from '/src/assets/img/minus.svg';
+import d2Img from '/src/assets/img/d2.svg';
+import d4Img from '/src/assets/img/d6.svg';
+import d6Img from '/src/assets/img/plus.svg';
+import d8Img from '/src/assets/img/minus.svg';
+import d10Img from '/src/assets/img/d2.svg';
+import d12Img from '/src/assets/img/d6.svg';
+import d20Img from '/src/assets/img/d6.svg';
+
+const images: Record<string, string> = {
+    plus: plusImg,
+    minus: minusImg,
+    d2: d2Img,
+    d4: d4Img,
+    d6: d6Img,
+    d8: d8Img,
+    d10: d10Img,
+    d12: d12Img,
+    d20: d20Img,
+    d100: d10Img
+}
 
 interface Roll {
     value: number
@@ -22,19 +44,17 @@ function Field({total, history, onReset, onInc, onDec}: Props) {
         <div className={styles.container}>
             <div className={styles.history}>
                 {history.slice().reverse().map((roll, index) => {
-                    let imgName
+                    let imgSrc
 
                     if (roll.magnitude === 'manual') {
-                        imgName = roll.type === 'plus' ? 'plus' : 'minus'
-                    } else if (roll.magnitude === 100) {
-                        imgName = 'd10'
+                        imgSrc = roll.type === 'plus' ? images.plus : images.minus
                     } else {
-                        imgName = `d${roll.magnitude}`
+                        imgSrc = images[`d${roll.magnitude}`]
                     }
 
                     return (
                         <div key={index} className={styles.historyWrapper}>
-                            <img className={styles.historyImg} src={`/img/${imgName}.svg`} alt=''/>
+                            <img className={styles.historyImg} src={imgSrc} alt=''/>
                             <span className={styles.historyRollValue}>{roll.value}</span>
                         </div>
                     )
